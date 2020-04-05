e9b96e15215a4509f6ee74b75e62fda1d9592248
Szymon Rzosinski <szymonrzosinski@gmail.com>
2020-04-05 18:41:58 +0200

    Masz JT

--- Assets/scripts/InteractionObject.cs
+++ Assets/scripts/InteractionObject.cs
@@ -15,22 +15,26 @@ public class InteractionObject : MonoBehaviour {
     [SerializeField] UnityEvent SonEvent;
     [SerializeField] UnityEvent MotherEvent;
     string[] triggeredDialog;
+    [SerializeField] bool MatkaInter = true, SynInter = true;
     DialogueSystemFromHell dsfh;
+    int mL = 0, sL = 0;
     void Start () {
+        mL=MatkaInter?9:0;
+        sL=SynInter?8:0;
         dsfh = FindObjectOfType<DialogueSystemFromHell> ();
     }
 
     public void OnTriggerEnter2D (Collider2D col) {
-        if (col.gameObject.tag == "Player") {
-            if(transform.childCount > 0)
-            transform.GetChild (0).gameObject.SetActive (true);
+        if (col.gameObject.tag == "Player" && (col.gameObject.layer == mL|| col.gameObject.layer == sL)) {
+            if (transform.childCount > 0)
+                transform.GetChild (0).gameObject.SetActive (true);
             col.gameObject.SendMessage ("SetInteractionObject", this);
         }
     }
     public void OnTriggerExit2D (Collider2D col) {
-        if (col.gameObject.tag == "Player") {
-            if(transform.childCount > 0)
-            transform.GetChild (0).gameObject.SetActive (false);
+        if (col.gameObject.tag == "Player" && (col.gameObject.layer == mL|| col.gameObject.layer == sL)) {
+            if (transform.childCount > 0)
+                transform.GetChild (0).gameObject.SetActive (false);
             col.gameObject.SendMessage ("ClearInteractionObject");
         }
     }
@@ -56,20 +60,20 @@ public class InteractionObject : MonoBehaviour {
     public void OnPlayerAction (Actor type) {
         switch (type) {
             case Actor.SON:
-                if(dialogSequenceSon.Length>0){
+                if (dialogSequenceSon.Length > 0) {
                     triggeredDialog = dialogSequenceSon;
-                    i=0;
-                    dialog();
+                    i = 0;
+                    dialog ();
                 }
-                SonEvent.Invoke();
+                SonEvent.Invoke ();
                 break;
             case Actor.MOTHER:
-                if(dialogSequenceMother.Length>0){
+                if (dialogSequenceMother.Length > 0) {
                     triggeredDialog = dialogSequenceMother;
-                    i=0;
-                    dialog();
+                    i = 0;
+                    dialog ();
                 }
-                MotherEvent.Invoke();
+                MotherEvent.Invoke ();
                 break;
         }
     }
