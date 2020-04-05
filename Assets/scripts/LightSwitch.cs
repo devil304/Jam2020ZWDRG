de251a1615d8c63a6abf1c6ff1a773cbfd7450e8
Tylyps <jakubb.chmielewskii@gmail.com>
2020-04-05 23:56:14 +0200

    Changes on P2 and lightong switch

--- Assets/scripts/LightSwitch.cs
+++ Assets/scripts/LightSwitch.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LightSwitch : MonoBehaviour {
-    [SerializeField] GameObject Light;
+    [SerializeField] GameObject Light = null;
+    [SerializeField] AudioClip LightOn = null;
+    [SerializeField] AudioClip LightOff = null;
+    public AudioSource audioSource = null;
+    public float volume = 0.3f;
 
     public void ToggleLight() {
-        Light.SetActive(!Light.gameObject.activeInHierarchy);
+        PlaySound(!Light.activeInHierarchy);
+        Light.SetActive(!Light.activeInHierarchy);
+    }
+
+    public void PlaySound(bool onOff) {
+        if (onOff && LightOn != null) {
+            audioSource.PlayOneShot(LightOn, volume);
+        } else if (LightOff != null) {
+            audioSource.PlayOneShot(LightOff, volume);
+        }
     }
 
 }
